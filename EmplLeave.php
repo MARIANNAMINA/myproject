07f42b79d57e37d1a966db3cc2303b87d2ee1fce
MariaKakoulli <31859349+MariaKakoulli@users.noreply.github.com>
2017-11-28 07:45:24 +0200

    Add files via upload

--- EmplLeave.php
+++ EmplLeave.php
@@ -12,6 +12,9 @@ $Desc=$_POST['desc'];
 $DateFrom=date("Y-m-d", strtotime($DateFrom));
 $DateTo=date("Y-m-d", strtotime($DateTo));
 
+$sql2= "SELECT username FROM staff WHERE username='$username'";
+$resultCheck=mysqli_num_rows(mysqli_query($conn,$sql2));
+if ( $resultCheck>0){
 $sql = "INSERT INTO leavereq (username,DateTo,DateFrom,Days,Description,Status) VALUES ('$username','$DateTo','$DateFrom',DATEDIFF('$DateTo','$DateFrom'),'$Desc','Waiting')";
 
 if(!mysqli_query($conn,$sql)){
@@ -25,6 +28,13 @@ else{echo '<script type="text/javascript">
 	window.location.replace("EmployeeDashboard.html");
 	</script>';}
 
+}
+else {
+	echo '<script type="text/javascript">
+	window.alert("NOT INSERTED"); 
+	window.location.replace("Leave Request_css.html");
+	</script>';
+}
 }
 
 else {echo '<script type="text/javascript">alert("NOT FOUND"); 
