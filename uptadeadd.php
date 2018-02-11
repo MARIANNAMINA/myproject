8ac68f77cc3fa78de86ab2958c01c1ee8659062e
mmina001 <31961648+mmina001@users.noreply.github.com>
2018-02-11 17:46:44 +0200

    connect with database

--- uptadeadd.php
+++ uptadeadd.php
@@ -3,27 +3,45 @@ session_start();
 if (isset($_POST['save'])){
 	include 'db.php';
 	
-$username=$_POST['username'];
-$password=$_POST['password'];
-$Name=$_POST['name'];
-$Surname=$_POST['surname'];
-$Date=$_POST['birthday'];
+$Username=$_POST['Username'];
+$Password = $_POST['Password'];
+//$Hashed_Password = password_hash($Password, PASSWORD_DEFAULT);
+$ID=$_POST['ID'];
+$SSN=$_POST['SSN'];
+$FirstName=$_POST['FirstName'];
+$LastName=$_POST['LastName'];
+$DepartmentNum=$_POST['DepartmentNum'];
+$Role=$_POST['Role'];
+$Salary=$_POST['Salary'];
+$SalaryType=$_POST['SalaryType'];
+$Phone=$_POST['Phone'];
+$EmergencyPhone=$_POST['EmergencyPhone'];
+$Email=$_POST['Email'];
+$Country=$_POST['Country'];
+$Address=$_POST['Address'];
+$DateofBirth=$_POST['DateofBirth'];
+$Gender=$_POST['Gender'];
+$Manager=$_POST['Manager'];
+$UsernameManager=$_POST['name'];
 
-
-$sql = "INSERT INTO staff (username,password,Name,Surname,birthdate) VALUES ('$username','$password','$Name','$Surname','$Date')";
+$sql = "INSERT INTO Employee (Username,Password,ID,SSN,Name,Surname,NumDept,Role,Salary,SalaryType,Phone,EmergencyPhone,Country,Address,Birthdate,Gender,IsManager,UsernameManager) VALUES ('$Username','$Password','$ID','$SSN','$FirstName','$LastName','$DepartmentNum','$Role','$Salary','$SalaryType','$Phone','$EmergencyPhone','$Country','$Address','$DateofBirth','$Gender','$Manager','$UsernameManager')";
 
 if(!mysqli_query($conn,$sql)){
-	echo '<script type="text/javascript">window.alert("NOT INSERTED"); 
-	window.location.replace("add_employee.php");
+	echo '<script type="text/javascript">
+	window.alert("NOT INSERTED"); 
+	window.location.replace("add_employee.html");
 	</script>';
 }
-else{echo '<script type="text/javascript">window.alert("NSERTED CORRECTLY"); 
+else{echo '<script type="text/javascript">
+	window.alert("INSERTED CORRECTLY"); 
 	window.location.replace("manager_dashboard.html");
 	</script>';}
 
 }
-else {echo '<script type="text/javascript">alert("NOT FOUND"); 
-	window.location.replace("add_employee.html");
+
+else {echo '<script type="text/javascript">
+    window.alert("NOT FOUND"); 
+	window.location.replace("manager_dashboard.html");
 	</script>';
 exit();}
 ?>
