bb0ab0b97fd2a4fb3b21e90376084e14b3f20842
ekatsi03 <31961624+ekatsi03@users.noreply.github.com>
2018-02-17 14:57:03 +0200

    Add files via upload

--- function_login_manager.php
+++ function_login_manager.php
@@ -6,8 +6,6 @@ include 'db.php';
 $username=$_POST['name'];
 $password=$_POST['word'];
 
-
-
 $sql = "SELECT * FROM Employee WHERE username='$username' AND password='$password'";
 $result=mysqli_query($conn,$sql);
 $resultCheck=mysqli_num_rows($result);
@@ -18,30 +16,25 @@ if ($resultCheck < 1){
         </script>';
 exit();}
 else { if ($row=mysqli_fetch_assoc($result)){
-        $isManager = "SELECT isManager FROM Employee WHERE username='$username' AND password='$password'";
-        if($isManager == 1){
+        $man = mysqli_query($conn,"SELECT IsManager FROM Employee WHERE username='$username' AND password='$password'");
+        $manager = mysqli_fetch_row($man);
+        if($manager[0] == 1){
                 echo '<script type="text/javascript">window.alert("LOGIN SUCCESSFULLY");
-                        window.location.replace("clock_in_manager.html");
-                        </script>';
+                window.location.replace("clock_in_manager.html");
+                </script>';
         }else{
                 echo '<script type="text/javascript">window.alert("LOGIN SUCCESSFULLY");
-                        window.location.replace("clock_in_employee.html");
-                        </script>';
-
+                window.location.replace("clock_in_employee.html");
+                </script>';
         }
-
 }
-
         else {
 echo "wrong password";}
         $pwscheck=password_verify($password,$row['password']);
 
-
-
 }
 }
 else {echo '<script type="text/javascript">alert("NOT FOUND");
-        window.location.replace("login_manager.php");
+        window.location.replace("index.html");
         </script>';
 exit();}
-?>
