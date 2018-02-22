f35fd802c0d1b9c3fc93baeccabb8859f4b3a10f
mkoupp02 <31859288+mkoupp02@users.noreply.github.com>
2018-02-22 23:21:53 +0200

    leave request manager and employee

--- employee_leave.php
+++ employee_leave.php
@@ -2,18 +2,19 @@
 session_start();
 if (isset($_POST['continue'])){
 	include 'db.php';
-
-
+	
 $username=$_SESSION['username'];
 
 $DateFrom=$_POST['From'];
 $DateTo=$_POST['To'];
+
 $Desc=$_POST['desc'];
 
 $DateFrom=date("Y-m-d", strtotime($DateFrom));
 $DateTo=date("Y-m-d", strtotime($DateTo));
 
 $sql2= "SELECT Username FROM Employee WHERE Username='$username'";
+
 $resultCheck=mysqli_num_rows(mysqli_query($conn,$sql2));
 
 $sql = "INSERT INTO `Leave`(`Reason`, `ToDate`, `FromDate`, `Username`) VALUES ('$Desc','$DateTo','$DateFrom','$username')";
@@ -21,22 +22,19 @@ $sql = "INSERT INTO `Leave`(`Reason`, `ToDate`, `FromDate`, `Username`) VALUES (
 if(!mysqli_query($conn,$sql)){
 	echo '<script type="text/javascript">
 	window.alert("NOT INSERTED"); 
-	window.location.replace("leave_request_manager.html");
+	window.location.replace("leave_request_employee.html");
 	</script>';
 }
 else{echo '<script type="text/javascript">
 	window.alert("INSERTED CORRECTLY"); 
-	window.location.replace("manager_dashboard.html");
+	window.location.replace("EmployeeDashboard.html");
 	</script>';}
 
 }
 
 
-else {echo '<script type="text/javascript">alert("SUCH FILE DOES NOT EXIST"); 
-	window.location.replace("delete_employee.html");
+else {echo '<script type="text/javascript">alert("NOT FOUND"); 
+	window.location.replace("leave_request_employee.html");
 	</script>';
-	
-
 exit();}
-
 ?>
