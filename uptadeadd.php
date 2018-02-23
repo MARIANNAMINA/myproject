7f0b919813c2a1432ce841a3b57e137688ba6ad3
mmina001 <31961648+mmina001@users.noreply.github.com>
2018-02-23 21:46:02 +0200

    function of add employee

--- uptadeadd.php
+++ uptadeadd.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
+
 if (isset($_POST['save'])){
-	include 'db.php';
-	
+        include 'db.php';
+
+//$Hashed_Password = password_hash($Password, PASSWORD_DEFAULT);
+
 $Username=$_POST['Username'];
 $Password = $_POST['Password'];
 //$Hashed_Password = password_hash($Password, PASSWORD_DEFAULT);
@@ -10,9 +13,10 @@ $ID=$_POST['ID'];
 $SSN=$_POST['SSN'];
 $FirstName=$_POST['FirstName'];
 $LastName=$_POST['LastName'];
-$DepartmentNum=$_POST['DepartmentNum'];
+$DepartmentNum = $_POST['DepartmentNum'];
+$CountryNumber = $_POST['CountryNumber'];
 $Role=$_POST['Role'];
-$Salary=$_POST['Salary'];
+$Salary= $_POST['Salary'];
 $SalaryType=$_POST['SalaryType'];
 $Phone=$_POST['Phone'];
 $EmergencyPhone=$_POST['EmergencyPhone'];
@@ -21,27 +25,114 @@ $Country=$_POST['Country'];
 $Address=$_POST['Address'];
 $DateofBirth=$_POST['DateofBirth'];
 $Gender=$_POST['Gender'];
-$Manager=$_POST['Manager'];
-$UsernameManager=$_POST['name'];
+$UsernameManager=$_SESSION['username'];
+$Manager=0;
+if(isset($_POST['Manager'])){
+$Manager=1;
+}
 
-$sql = "INSERT INTO Employee (Username,Password,ID,SSN,Name,Surname,NumDept,Role,Salary,SalaryType,Phone,EmergencyPhone,Country,Address,Birthdate,Gender,IsManager,UsernameManager) VALUES ('$Username','$Password','$ID','$SSN','$FirstName','$LastName','$DepartmentNum','$Role','$Salary','$SalaryType','$Phone','$EmergencyPhone','$Country','$Address','$DateofBirth','$Gender','$Manager','$UsernameManager')";
+if(strcmp($Gender,"Female")==0){
+        $Gender="F";
+}else{
+        $Gender="M";
+}
 
+if(strcmp($SalaryType,"Fixed")==0){
+        $SalaryType="F";
+}else if(strcmp($SalaryType,"Part Time")==0){
+        $SalaryType="P";
+}else{
+        $SalaryType="FO";
+}
+list($y, $m, $d) = explode('-', $DateofBirth);
+if(!checkdate($m, $d, $y)) {
+         echo '<script type="text/javascript">
+        window.alert("NOT CORRECT DATE OF BIRTH FORMAT");
+        window.location.replace("add_employee.html");
+        </script>';
+        exit();
+}
+
+if(!is_numeric($DepartmentNum)){
+   echo '<script type="text/javascript">
+        window.alert("DEPARTMENT NUMBER MUST BE AN INTEGER NUMBER!");
+        window.location.replace("add_employee.html");
+        </script>';
+        exit();
+}
+
+if(!is_numeric($CountryNumber)){
+   echo '<script type="text/javascript">
+        window.alert("COUNTRY NUMBER MUST BE AN INTEGER NUMBER!");
+        window.location.replace("add_employee.html");
+        </script>';
+        exit();
+}
+
+if(!is_numeric($Salary)){
+   echo '<script type="text/javascript">
+        window.alert("SALARY MUST BE A NUMBER!");
+        window.location.replace("add_employee.html");
+        </script>';
+        exit();
+}
+
+
+$sqlDeptNum="SELECT NumberDept FROM Department WHERE NumberDept={$DepartmentNum}";
+$sqlCountryNum="SELECT CountryNum FROM CorporateHeadquarter WHERE CountryNum={$CountryNumber}";
+$resultCountryNum=mysqli_query($conn,$sqlCountryNum);
+$resultDeptNum=mysqli_query($conn,$sqlDeptNum);
+if((!($resultDeptNum))||(!($resultCountryNum))){
+        echo '<script type="text/javascript">
+        window.alert("ERROR CONNECTION WITH DATABASE");
+        window.location.replace("add_employee.html");
+        </script>';
+
+
+}else{
+$rowsDeptNum = mysqli_num_rows($resultDeptNum);
+$rowsCountryNum = mysqli_num_rows($resultCountryNum);
+if($rowsCountryNum==0){
+   echo '<script type="text/javascript">
+        window.alert("COUNTRY NUMBER DOES NOT EXIST IN DATABASE");
+        window.location.replace("add_employee.html");
+        </script>';
+}
+else if($rowsDeptNum==0){
+         echo '<script type="text/javascript">
+        window.alert("DEPARTMENT NUMBER DOES NOT EXIST IN DATABASE");
+        window.location.replace("add_employee.html");
+        </script>';
+
+}else if(($rowsCountryNum!=0) && ($rowsDeptNum!=0)){
+$increaseEmployees="UPDATE Department SET NumEmployees=(NumEmployees+1) WHERE NumberDept={$DepartmentNum}";
+if(!mysqli_query($conn,$increaseEmployees)){
+        echo '<script type="text/javascript">
+        window.alert("ERROR CONNECTING WITH DATABASE");
+        window.location.replace("add_employee.html");
+        </script>';
+}else{
+$sql = "INSERT INTO Employee (CountryNumber,Email,NumDept,IsManager,Username,Password,ID,SSN,Name,Surname,Role,Salary,SalaryType,Phone,EmergencyPhone,Country,Address,Birthdate,Gender,UsernameManager) VALUES ('$CountryNumber','$Email','$DepartmentNum','$Manager','$Username','$Password','$ID','$SSN','$FirstName','$LastName','$Role','$Salary','$SalaryType','$Phone','$EmergencyPhone','$Country','$Address','$DateofBirth','$Gender','$UsernameManager')";
 
 if(!mysqli_query($conn,$sql)){
-	echo '<script type="text/javascript">
-	window.alert("NOT INSERTED"); 
-	window.location.replace("add_employee.html");
-	</script>';
-}
-else{echo '<script type="text/javascript">
-	window.alert("INSERTED CORRECTLY"); 
-	window.location.replace("manager_dashboard.html");
-	</script>';}
+ echo '<script type="text/javascript">
+        window.alert("ERROR CONNECTING WITH DATABASE");
+        window.location.replace("add_employee.html");
+        </script>';
 
 }
+else{echo '<script type="text/javascript">
+        window.alert("INSERTED CORRECTLY");
+        window.location.replace("manager_dashboard.html");
+        </script>';}
+}
+}
+}
+}
 else {echo '<script type="text/javascript">
-    window.alert("NOT FOUND"); 
-	window.location.replace("manager_dashboard.html");
-	</script>';
+    window.alert("NOT FOUND");
+        window.location.replace("manager_dashboard.html");
+        </script>';
 exit();}
 ?>
+
