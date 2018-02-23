ca5bda0dbdd00c69c5282da9ef966d0b5e2280b8
LouizaAgroti1 <31855678+LouizaAgroti1@users.noreply.github.com>
2018-02-23 13:44:42 +0200

    delete operation

--- delete_employee.php
+++ delete_employee.php
@@ -6,14 +6,13 @@ if (isset($_POST['save'])){
 $Username=$_POST['Username'];
 
 
-/*$sql2= "SELECT username FROM staff WHERE username='$username'";
+$sql2= "SELECT Username FROM Employee WHERE Username='$Username'";
 $resultCheck=mysqli_num_rows(mysqli_query($conn,$sql2));
 
-$sql = "DELETE FROM staff WHERE username='$username' ";
+$sql = "DELETE FROM Employee WHERE Username='$Username' ";
 
 
 $result=mysqli_query($conn,$sql);
-
 
 
 if(!mysqli_query($conn,$sql) || $resultCheck<1 ){
@@ -24,48 +23,18 @@ if(!mysqli_query($conn,$sql) || $resultCheck<1 ){
 else{echo '<script type="text/javascript">window.alert("DELETED SUCCESSFULLY"); 
 	window.location.replace("manager_dashboard.html");
 	</script>';
+//	$sql3 = "SELECT * FROM Employee WHERE Username='$Username'";
+//	$sql4 = "INSERT INTO DeletedEmployee (Username,Password,ID,SSN,Name,Surname,NumDept,Role,Salary,SalaryType,Phone,EmergencyPhone,Country,Address,Birthdate,Gender,IsManager,UsernameManager) VALUES ('$Username','$Password','$ID','$SSN','$FirstName','$LastName','$DepartmentNum','$Role','$Salary','$SalaryType','$Phone','$EmergencyPhone','$Country','$Address','$DateofBirth','$Gender','$Manager','$UsernameManager')";
+//	$result1 = mysql_query($conn,$sql4);
 }
 
 }
 
 else {echo '<script type="text/javascript">alert("SUCH FILE DOES NOT EXIST"); 
-	window.location.replace("Delete Employee.html");
-	</script>';
-	*/
-
-
-
-
-
-
-$sql = "DELETE FROM Employee WHERE Username='$Username' ";
-$sql2= "SELECT Username FROM Employee WHERE Username='$Username'";
-$result=mysqli_query($conn,$sq2);
-$resultCheck=mysqli_num_rows(mysqli_query($conn,$sql2));
-
-//$sql = "DELETE FROM Employee WHERE Username='$Username' ";
-
-//$result=mysqli_query($conn,$sql);
-
-if(!result || $resultCheck<1 ){
-	echo '<script type="text/javascript">
-	window.alert("NOT USER FOUND"); 
-	window.location.replace("delete_employee.html");
-	</script>';
-}
-else{echo '<script type="text/javascript">
-	window.alert("DELETED SUCCESSFULLY"); 
-	window.location.replace("manager_dashboard.html");
-	</script>';
-}
-
-}
-
-else {echo '<script type="text/javascript">
-	window.alert("SUCH FILE DOES NOT EXIST"); 
 	window.location.replace("delete_employee.html");
 	</script>';
 	
-	
+
 exit();}
+
 ?>
