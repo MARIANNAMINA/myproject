449f456da4eadb0a7baab30e2c89ab1b4c65e162
ekatsi03 <31961624+ekatsi03@users.noreply.github.com>
2018-04-18 21:52:40 +0300

    Add files via upload

--- Employee/Contact Us/contact_employee.php
+++ Employee/Contact Us/contact_employee.php
@@ -309,170 +309,140 @@ include('contact.php');
 </style>
 	
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-  </head>
-  <body>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body>
 		
-		<div class="header">
+<div class="header">
 
-		<form action="logout_employee.php" method="post" id=form_id4>	 	
-			<button onclick="myFunction4()" name="LogOutButton" id="LogOutButton" class="logout">LogOut</button>
-	 	</form>  
+	<form action="logout_employee.php" method="post" id=form_id4>	 	
+		<button onclick="myFunction4()" name="LogOutButton" id="LogOutButton" class="logout">LogOut</button>
+	</form>  
 
-
-
-		<div class="logo">
+	<div class="logo">
 		<a href="clock_in_employee.html">
 			<img src="https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAgWAAAAJDhlYjE0YzE2LWVjOTItNGU1OS04N2M2LWI3YTZkNzIzNTljMw.png" width="100" height="100"> 
 		</a>
 		
 		<ul>
-		<label class="nav">
-		<li><a href="clock_in_employee.html">Home</a></li>
-		<li class="dropdown">
-			<a href="javascript:void(0)" class="dropbtn">Language</a>
-			<div class="dropdown-content">
-				<a href="#">Ελληνικά</a>
-				<a href="#">English</a>
-				<a href="#">Norsk</a>
-				<a href="#">Polski</a>
-				<a href="#">Deutsch</a>
-				<a href="#">Svenska</a>
-			</div>
-		</li>
-		<li><a href="contact_employee.php" style="color:orange;text-decoration: underline">Contact Us</a></li>
-		</label>
-	</ul>
-
+			<label class="nav">
+				<li><a href="clock_in_employee.html">Home</a></li>
+				<li class="dropdown">
+					<a href="javascript:void(0)" class="dropbtn">Language</a>
+					<div class="dropdown-content">
+						<a href="#">Ελληνικά</a>
+						<a href="#">English</a>
+						<a href="#">Norsk</a>
+						<a href="#">Polski</a>
+						<a href="#">Deutsch</a>
+						<a href="#">Svenska</a>
+					</div>
+				</li>
+				<li><a href="contact_employee.php" style="color:orange;text-decoration: underline">Contact Us</a></li>
+			</label>
+		</ul>
 
 	</div> 
-		</div>
-	<br>
+</div>
+<br>
 <div>
 
-		<form method="post" class="username" id="form_id" action="<?php $_SERVER['PHP_SELF']; ?>">
+	<form method="post" class="username" id="form_id" action="<?php $_SERVER['PHP_SELF']; ?>">
 				
-				<label  id="label" for="textinput3" ><b><label style="color:red">*</label>TO :</b></label> 
-				<div class = "input1">
-					<input name="To" id="To" type="text" value="<?php echo "$TO"; ?>" placeholder=""  required="">
-				</div> 
-				<span style="margin-left:360px" class="error"><?php echo "$to_error"; ?></span>
+		<label  id="label" for="textinput3" ><b><label style="color:red">*</label>TO :</b></label> 
+		<div class = "input1">
+			<input name="To" id="To" type="text" value="<?php echo "$TO"; ?>" placeholder=""  required="">
+		</div> 
+		<span style="margin-left:360px" class="error"><?php echo "$to_error"; ?></span>
 				
-				<br>
-				<label  id="label" for="textinput3" ><b><label style="color:red">*</label>SUBJECT :</b></label> 
-				<div class = "input1">
-					<input name="Subject" id="Subject" type="text" value="<?php echo "$SUBJECT"; ?>" placeholder=""  required="">
-				</div>  
-				<span style="margin-left:360px" class="error"><?php echo "$subject_error"; ?></span>
+		<br>
+		<label  id="label" for="textinput3" ><b><label style="color:red">*</label>SUBJECT :</b></label> 
+		<div class = "input1">
+			<input name="Subject" id="Subject" type="text" value="<?php echo "$SUBJECT"; ?>" placeholder=""  required="">
+		</div>  
+		<span style="margin-left:360px" class="error"><?php echo "$subject_error"; ?></span>
 				
-				<br>
-				<label id="label" for="textinput3"><b><label style="color:red">*</label>Description :</b></label>  
-				<div class = "input1">
-					<input name="Description" id="Description" type="text" value="<?php echo "$DESCRIPTION"; ?>" placeholder=""  required="">
-				</div>
-				<span style="margin-left:360px" class="error"><?php echo "$descripiton_error"; ?></span>
+		<br>
+		<label id="label" for="textinput3"><b><label style="color:red">*</label>Description :</b></label>  
+		<div class = "input1">
+			<input name="Description" id="Description" type="text" value="<?php echo "$DESCRIPTION"; ?>" placeholder=""  required="">
+		</div>
+		<span style="margin-left:360px" class="error"><?php echo "$descripiton_error"; ?></span>
 
-  <div>
-  <br>
-  	<div class="paragraph2">
-		<!--	<label onclick="myFunction1()" class="buttonstyle1">Cancel</label>
-			<button onclick="myFunction2()" class="buttonstyle" name="Send">Send</button>-->
-			
-		<button class="buttonstyle1" id="cancel" name="cancel">Cancel</button>	
-		<button class="buttonstyle" id="send" name="send">Send</button>
-	    
+		<div>
+			<br>
+			<div class="paragraph2">
+				<button class="buttonstyle1" id="cancel" name="cancel">Cancel</button>	
+				<button class="buttonstyle" id="send" name="send">Send</button>
+			</div>				
+		</div>
 	</form>
-	</div>		
-			
-	</div>
-
 </div>
 		  
 <div  id="welcomeDiv" style="display:none" class="confirm_box">
-  <div class="overlay"></div>
-   <div class="confirm_model">
-     <div class="model">
-	 <div class="header">
-         <h1 class="title">
-           Do you want to send the message?
-         </h1>
-       </div>
-       <div class="content">
-         <div class="buttons_container">
-           <button class="confirm button" id="yes" name="yes">Yes</button>
-           <button class="deny button" id="no" name="no">No</button>
-         </div>
-       </div>
-     </div>
-  </div>
+	<div class="overlay"></div>
+	<div class="confirm_model">
+		<div class="model">
+			<div class="header">
+				<h1 class="title">
+					Do you want to send the message?
+				</h1>
+			</div>
+			<div class="content">
+				<div class="buttons_container">
+					<button class="confirm button" id="yes" name="yes">Yes</button>
+					<button class="deny button" id="no" name="no">No</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div  id="cancelDiv" style="display:none" class="confirm_box">
-  <div class="overlay"></div>
-   <div class="confirm_model">
-     <div class="model">
-	 <div class="header">
-         <h1 class="title">
-           Do you want to leave this page?
-         </h1>
-       </div>
-       <div class="content">
-         <div class="buttons_container">
-           <button class="confirm button" id="yes2" name="yes2">Yes</button>
-           <button class="deny button" id="no2" name="no2">No</button>
-         </div>
-       </div>
-     </div>
-  </div>
+	<div class="overlay"></div>
+	<div class="confirm_model">
+		<div class="model">
+			<div class="header">
+				<h1 class="title">
+					Do you want to leave this page?
+				</h1>
+			</div>
+			<div class="content">
+				<div class="buttons_container">
+					<button class="confirm button" id="yes2" name="yes2">Yes</button>
+					<button class="deny button" id="no2" name="no2">No</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-			
-
-  			
-	<script>
-		/*function myFunction1() {
-			if(confirm('Do you want to leave this page?')){
-				
-				return true;
-			}
-
-		}
-		function myFunction2() {
-			if(confirm('Do you want to send the message?')){
-				document.getElementById("form_id").submit();
-			}
-			
-		}
-
-		function myFunction4() {
-        document.getElementById("form_id4").submit();
-        }*/
-$("#send").click(function ( event ) { 
-    event.preventDefault();
-	document.getElementById('welcomeDiv').style.display = "block";
-	$("#yes").click(function ( event ) { 
-		document.getElementById('welcomeDiv').style.display = "none";
-		document.getElementById('form_id').submit();
-	});
-	$("#no").click(function ( event ) { 
-		document.getElementById('welcomeDiv').style.display = "none";
 		
+<script>
+	$("#send").click(function ( event ) { 
+		event.preventDefault();
+		document.getElementById('welcomeDiv').style.display = "block";
+		$("#yes").click(function ( event ) { 
+			document.getElementById('welcomeDiv').style.display = "none";
+			document.getElementById('form_id').submit();
+		});
+		$("#no").click(function ( event ) { 
+			document.getElementById('welcomeDiv').style.display = "none";
+		});
 	});
-});
 
-$("#cancel").click(function ( event ) { 
-    event.preventDefault();
-	document.getElementById('cancelDiv').style.display = "block";
-	$("#yes2").click(function ( event ) { 
-		document.getElementById('cancelDiv').style.display = "none";
-	    window.location.replace("clock_in_employee.html");
+	$("#cancel").click(function ( event ) { 
+		event.preventDefault();
+		document.getElementById('cancelDiv').style.display = "block";
+		$("#yes2").click(function ( event ) { 
+			document.getElementById('cancelDiv').style.display = "none";
+			window.location.replace("clock_in_employee.html");
+		});
+		$("#no2").click(function ( event ) { 
+			document.getElementById('cancelDiv').style.display = "none";
+		});
 	});
-	$("#no2").click(function ( event ) { 
-		document.getElementById('cancelDiv').style.display = "none";
-		//document.getElementById('form_id').submit();
-	});
-});
 </script>
   
-  </body>
-  </html>
+</body>
+</html>
