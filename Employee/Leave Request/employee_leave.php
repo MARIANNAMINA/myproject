6a70e8f147d00bf0977016b400845265e208ffac
mkoupp02 <31859288+mkoupp02@users.noreply.github.com>
2018-04-18 22:43:31 +0300

    Add files via upload

--- Employee/Leave Request/employee_leave.php
+++ Employee/Leave Request/employee_leave.php
@@ -1,10 +1,36 @@
+<!--
+- Author: Maria Kouppi
+-
+- This is a php file for leave request functionality that an employee can do. If the employees click the sumbit button 
+-->
 <?php
 session_start();
+
+/* 
+* Checks whether the date which the employee wants to start or
+* leave the permit is before this date(today) or not. 
+*
+* @param $date - the date which the employee wants to start or
+* 					leave
+*
+*/
+function checkDate($date){
+	if( strtotime($date) <= strtotime('now') ) {
+		
+		return false;
+	} else{
+		return true;
+	}
+	
+}
+
+// click the submit button
 if (isset($_POST['continue'])){
 	include 'db.php';
 	
 $username=$_SESSION['username'];
 
+// get the values of text box
 $DateFrom=$_POST['From'];
 $DateTo=$_POST['To'];
 
@@ -13,27 +39,44 @@ $Desc=$_POST['desc'];
 $DateFrom=date("Y-m-d", strtotime($DateFrom));
 $DateTo=date("Y-m-d", strtotime($DateTo));
 
-$sql2= "SELECT Username FROM Employee WHERE Username='$username'";
+//call checkDate function
+$flag = checkDate($DateFrom);
+$flag1 = checkDate($DateTo);
 
-$resultCheck=mysqli_num_rows(mysqli_query($conn,$sql2));
-
-$sql = "INSERT INTO `Leave`(`Reason`, `ToDate`, `FromDate`, `Username`) VALUES ('$Desc','$DateTo','$DateFrom','$username')";
-
-if(!mysqli_query($conn,$sql)){
+// if function returns false, print error message
+if(!$flag || !$flag1) {
 	echo '<script type="text/javascript">
-	window.alert("NOT INSERTED"); 
+	var d = new Date();
+	window.alert("The date must be after " + d.getDate() + "/" + d.getMonth() + "/"  + d.getFullYear()); 
 	window.location.replace("leave_request_employee.html");
 	</script>';
-}
-else{echo '<script type="text/javascript">
-	window.alert("INSERTED CORRECTLY"); 
-	window.location.replace("EmployeeDashboard.html");
-	</script>';}
+} else if (strtotime($DateTo) < strtotime($DateFrom)) { //the date which the leave starts should be before the date which the leave finishes
+		echo '<script type="text/javascript">
+			window.alert("The date on which the leave will end is after the date it starts! Try again please!");
+			window.location.replace("leave_request_employee.html");
+			</script>';
+} else {
 
-}
+	$sql2= "SELECT Username FROM Employee WHERE Username='$username'";
 
+	$resultCheck=mysqli_num_rows(mysqli_query($conn,$sql2));
 
-else {echo '<script type="text/javascript">alert("NOT FOUND"); 
+	// insert to the database
+	$sql = "INSERT INTO `Leave`(`Reason`, `ToDate`, `FromDate`, `Username`) VALUES ('$Desc','$DateTo','$DateFrom','$username')";
+
+	if(!mysqli_query($conn,$sql)){
+		echo '<script type="text/javascript">
+		window.alert("NOT INSERTED"); 
+		window.location.replace("leave_request_employee.html");
+		</script>';
+	}
+	else{echo '<script type="text/javascript">
+		window.alert("INSERTED CORRECTLY"); 
+		window.location.replace("EmployeeDashboard.html");
+		</script>';}
+
+	}
+} else {echo '<script type="text/javascript">alert("NOT FOUND"); 
 	window.location.replace("leave_request_employee.html");
 	</script>';
 exit();}
