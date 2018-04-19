19c61e53a99ec2bf57c209f2b9ba018ec23f1332
ekatsi03 <31961624+ekatsi03@users.noreply.github.com>
2018-04-20 01:28:25 +0300

    Add files via upload

--- Manager/Payroll Report/payroll_function.php
+++ Manager/Payroll Report/payroll_function.php
@@ -15,58 +15,82 @@ $file_name=$_POST['file_name'];
 $Username=$_SESSION['username'];
 if(isset($_POST['export_btn'])){
 	
-	//select data from database
-	$getData="SELECT SUM(TIME_TO_SEC(TIMEDIFF(AttendanceTime.ClockOut,AttendanceTime.ClockIn))-(AttendanceTime.BreakLength*60)) AS SecWorked,AttendanceTime.Username,AttendanceTime.BreakLength,Employee.ID,Employee.Name,Employee.Surname FROM AttendanceTime INNER JOIN Employee ON (AttendanceTime.Username=Employee.Username) WHERE (Date >= '$From' AND Date <= '$To') AND Employee.UsernameManager LIKE '$Username' GROUP BY AttendanceTime.Username";
-	
-	$sql_con=mysqli_query($conn,$getData);
-	$employee_data = array();  
-	if(!$sql_con){
-		echo '<script type="text/javascript">
-			window.alert("ERROR CONNECTION WITH DATABASE");
+	$DateFrom = date("Y-m-d", strtotime($From));
+    $DateTo = date("Y-m-d", strtotime($To));
+
+	if (strtotime($DateTo) < strtotime($DateFrom)) {
+        echo '<script type="text/javascript">
+			window.alert("The To field has to be after the From field!");
 			window.location.replace("payroll_report.html");
 			</script>';
-		exit();
-	}else{
-		//if format which the manager select is XML
-		if(strcmp($_POST['format_export'],"XML")==0){
-			$xml          = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-			$root_element = "Leave_Employee"; 
-			$xml         .= "<$root_element>";
-			if(mysqli_num_rows($sql_con)>0){
-				while($row = mysqli_fetch_assoc($sql_con)){
-					$xml .= "<Leave_Employee>";
-		 
-					//loop through each key,value pair in row
-					foreach($row as $key => $value){
-						//$key holds the table column name
-						$xml .= "<$key>";
-				 
-						//embed the SQL data in a CDATA element to avoid XML entity issues
-						$xml .= "<![CDATA[$value]]>";
-				 
-						//and close the element
-						$xml .= "</$key>";
-					}
-				 
-					$xml.="</Leave_Employee>";
-				}
-				   
-				$xml .= "</$root_element>";
-				 
-				//send the xml header to the browser
-				header ("Content-Type:text/xml");
-				 
-				//output the XML data
-				echo $xml;
-			}
-		}else{
-			//if the format which the manager select is JSON
-			while($row=mysqli_fetch_array($sql_con)){
-				$employee_data[] = array('Username'=> $row['Username'],'ID' => $row['ID'],'Name' => $row['Name'],'Surname' => $row['Surname'],'SecWorked' => $row['SecWorked']);
-			}
-			echo json_encode($employee_data);
-		}
+    }else if (strtotime($DateFrom) > strtotime('now')) {
+        echo '<script type="text/javascript">
+			var d = new Date();
+			window.alert("The date must be before " + d.getDate() + "/" + d.getMonth() + "/"  + d.getFullYear()); 
+			window.location.replace("payroll_report.html");
+			</script>';
+    } else if (strtotime($DateTo) > strtotime('now')) {
 
+        echo '<script type="text/javascript">
+			var d = new Date();
+			window.alert("The date must be before " + d.getDate() + "/" + d.getMonth() + "/"  + d.getFullYear()); 
+			window.location.replace("payroll_report.html");
+			</script>';
+
+    } else{
+		//select data from database
+		$getData="SELECT SUM(TIME_TO_SEC(TIMEDIFF(AttendanceTime.ClockOut,AttendanceTime.ClockIn))-(AttendanceTime.BreakLength*60)) AS SecWorked,AttendanceTime.Username,AttendanceTime.BreakLength,Employee.ID,Employee.Name,Employee.Surname FROM AttendanceTime INNER JOIN Employee ON (AttendanceTime.Username=Employee.Username) WHERE (Date >= '$From' AND Date <= '$To') AND Employee.UsernameManager LIKE '$Username' GROUP BY AttendanceTime.Username";
+		
+		$sql_con=mysqli_query($conn,$getData);
+		$employee_data = array();  
+		if(!$sql_con){
+			echo '<script type="text/javascript">
+				window.alert("ERROR CONNECTION WITH DATABASE");
+				window.location.replace("payroll_report.html");
+				</script>';
+			exit();
+		}else{
+			//if format which the manager select is XML
+			if(strcmp($_POST['format_export'],"XML")==0){
+				$xml          = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+				$root_element = "Leave_Employee"; 
+				$xml         .= "<$root_element>";
+				if(mysqli_num_rows($sql_con)>0){
+					while($row = mysqli_fetch_assoc($sql_con)){
+						$xml .= "<Leave_Employee>";
+			 
+						//loop through each key,value pair in row
+						foreach($row as $key => $value){
+							//$key holds the table column name
+							$xml .= "<$key>";
+					 
+							//embed the SQL data in a CDATA element to avoid XML entity issues
+							$xml .= "<![CDATA[$value]]>";
+					 
+							//and close the element
+							$xml .= "</$key>";
+						}
+					 
+						$xml.="</Leave_Employee>";
+					}
+					   
+					$xml .= "</$root_element>";
+					 
+					//send the xml header to the browser
+					header ("Content-Type:text/xml");
+					 
+					//output the XML data
+					echo $xml;
+				}
+			}else{
+				//if the format which the manager select is JSON
+				while($row=mysqli_fetch_array($sql_con)){
+					$employee_data[] = array('Username'=> $row['Username'],'ID' => $row['ID'],'Name' => $row['Name'],'Surname' => $row['Surname'],'SecWorked' => $row['SecWorked']);
+				}
+				echo json_encode($employee_data);
+			}
+
+		}
 	}
 }
 ?>
