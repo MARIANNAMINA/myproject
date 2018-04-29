886d577fa2c13ed7569689dca498b0df7512b658
mkoupp02 <31859288+mkoupp02@users.noreply.github.com>
2018-04-29 21:59:16 +0300

    Add files via upload

--- Manager/View Request/changeState_viewRequest.php
+++ Manager/View Request/changeState_viewRequest.php
@@ -1,36 +1,112 @@
 <?php
 session_start();
 
+include 'db.php';
 
+/**
+ * Manager click one of the buttons, and change the state in database for leave request
+ *
+ */
+function changeState()
+{
+    if (isset($_POST['Reject'])) {
+        $state = "r";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    include 'db.php';
+        echo '<script type="text/javascript">
+			window.alert("' . $state . '"); 
+			</script>';
+        //$array[] = $_SESSION['leave_data'];
 
-    $choice = $_POST['choice'];
-    echo '<script type="text/javascript">
-			window.alert("MARIA KOUPPi"); 
+        $row = $_SESSION['row'];
+        // This will evaluate to TRUE so the text will be printed.
+        if ($row == 0) {
+            echo '<script type="text/javascript">
+			window.alert("Empty!!"); 
 			
 			</script>';
-    $array[] = $_SESSION['leave_data'];
+        } else {
+            echo '<script type="text/javascript">
+			window.alert("' . $row . '"); 
+			
+			</script>';
 
-   // $row = $_GET['row'];
-
-
-    $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`='$State' WHERE `Leave`.`LeaveID` = '$array[$row]'";
-    if (!mysqli_query($conn, $sqlUpdate)) {
-        echo '<script type="text/javascript">
+            $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`='$state' WHERE `Leave`.`LeaveID` = 598";
+            if (!mysqli_query($conn, $sqlUpdate)) {
+                echo '<script type="text/javascript">
 			window.alert("Your change not insert in Database!" );		
 			window.location.replace("manager_view_request.php");
 			</script>';
-        exit();
-    } else {
-        echo '<script type="text/javascript">
+                exit();
+            } else {
+                echo '<script type="text/javascript">
 			window.alert("INSERTED CORRECTLY!"); 
 			window.location.replace("manager_view_request.php");
 			</script>';
+            }
+
+        }
+    } else if (isset($_POST['Accept'])) {
+
+
+        $state = "a";
+
+
+        echo '<script type="text/javascript">
+			window.alert("' . $state . '"); 
+			
+			</script>';
+        $array[] = $_SESSION['leave_data'];
+
+        // $row = $_GET['row'];
+
+
+        $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`='$state' WHERE `Leave`.`LeaveID` = '$array[$row]'";
+        if (!mysqli_query($conn, $sqlUpdate)) {
+            echo '<script type="text/javascript">
+			window.alert("Your change not insert in Database!" );		
+			window.location.replace("manager_view_request.php");
+			</script>';
+            exit();
+        } else {
+            echo '<script type="text/javascript">
+			window.alert("INSERTED CORRECTLY!"); 
+			window.location.replace("manager_view_request.php");
+			</script>';
+        }
+
+
+    } else if (isset($_POST['Pending'])) {
+
+
+        $state = "p";
+
+
+        echo '<script type="text/javascript">
+			window.alert("' . $state . '"); 
+			
+			</script>';
+        $array[] = $_SESSION['leave_data'];
+
+        // $row = $_GET['row'];
+
+
+        $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`='$state' WHERE `Leave`.`LeaveID` = '$array[$row]'";
+        if (!mysqli_query($conn, $sqlUpdate)) {
+            echo '<script type="text/javascript">
+			window.alert("Your change not insert in Database!" );		
+			window.location.replace("manager_view_request.php");
+			</script>';
+            exit();
+        } else {
+            echo '<script type="text/javascript">
+			window.alert("INSERTED CORRECTLY!"); 
+			window.location.replace("manager_view_request.php");
+			</script>';
+        }
+
+
     }
-
-
 }
 
+changeState();
 ?>
