9ac96c0d08d7304cda6e0128c2fedb4dc73a6443
mkoupp02 <31859288+mkoupp02@users.noreply.github.com>
2018-05-03 00:06:06 +0300

    Add files via upload

---
+++ Manager/Average Report/average_per_week.php
@@ -0,0 +1,197 @@
+<?php session_start();
+include('db.php');
+include('average_per_week_manager.php'); ?>
+
+<!--
+- Author: Maria Kouppi
+-
+- A manager can can report on the time worked by his employees. At the top of the page you will see
+- the menu with the other features you can do on this web page. The managers give the range of dates the managers
+- want to report, the name of file and the format which they want the report has(XML or JSON). When the fields are
+- complete, pressing the button 'Export’, depending on the choice in the format, the report exports either in XML
+- either in JSON.
+-
+-->
+<!doctype html>
+<html lang="en">
+<link rel="shortcut icon"
+      href="https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAgWAAAAJDhlYjE0YzE2LWVjOTItNGU1OS04N2M2LWI3YTZkNzIzNTljMw.png">
+<link rel="apple-touch-icon"
+      href="https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAgWAAAAJDhlYjE0YzE2LWVjOTItNGU1OS04N2M2LWI3YTZkNzIzNTljMw.png">
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>Statare LTD</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" type="text/css" href="average_report.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+          integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body>
+<!-- Header with the other features-->
+<div class="header">
+
+    <form action="logout_manager.php" method="post" id=form_id4>
+        <button onclick="myFunction4()" name="LogOutButton" id="LogOutButton" class="logout">LogOut</button>
+    </form>
+
+
+    <div class="logo">
+        <a href="manager_dashboard.html">
+            <img src="statare.png" alt="Statare logo" width="50%" height="50%">
+        </a>
+        <ul>
+            <label class="nav">
+                <li><a href="manager_dashboard.html">Home</a></li>
+                <li><a href="edit_profile_manager.php">Profile</a></li>
+                <li><a href="view_hours_manager.php">View Hours</a></li>
+                <li><a href="leave_request_manager.php">Leave Request</a></li>
+                <li><a href="average_per_week.php" style="color:orange;text-decoration: underline">Average Report</a>
+                </li>
+                <li><a href="payroll_report.php">Payroll Report</a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">My Employees</a>
+                    <div class="dropdown-content">
+                        <a href="add_employee.php">Add Employee</a>
+                        <a href="choose_employee.php">Edit Employee</a>
+                        <a href="delete_employee_.php">Delete Employee</a>
+                        <a href="employee_status_manager.php">Employee Status</a>
+                        <a href="leaveRequest.html">View Requests</a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Language</a>
+                    <div class="dropdown-content">
+                        <a href="#">Ελληνικά</a>
+                        <a href="#">English</a>
+                    </div>
+                </li>
+            </label>
+        </ul>
+
+
+    </div>
+</div>
+
+<div class="paragraph">
+
+    <p class="title_class"><b>Average Report</b></p>
+
+</div>
+<br>
+
+<form name="form_id" id="form_id" class="username" method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+    <!-- the range of dates-->
+    <label id="label" style="margin-right:6%"><b>From :</b></label>
+    <input id="textinput1" name="from_date" type="date" placeholder="" required="">
+
+    <br>
+    <br>
+    <label id="label" style="margin-right:8%"><b>To :</b></label>
+    <input id="textinput2" name="to_date" type="date" placeholder="" required="">
+
+    <br>
+    <br>
+    <!-- the name of file
+    <label id="label" for="textinput3"> <b>File name :</b></label>
+    <input id="textinput3" name="file_name" type="search" placeholder="" required="">
+    <br>
+    <br>-->
+    <!-- the format of report-->
+    <label style="margin-right:6%"><b>Format : </b></label>
+    <select name="format_export" id="format_export">
+        <option value="XML">XML</option>
+        <option value="JSON">JSON</option>
+    </select>
+
+    <br>
+    <br>
+
+    <button class="cancel_style" id="cancel" name="cancel">Cancel</button>
+    <button class="save_style" id="export_btn" name="export_btn">Export</button>
+</form>
+
+<!--pop up window -->
+<div id="exportDiv" style="display:none" class="confirm_box">
+    <div class="overlay"></div>
+    <div class="confirm_model">
+        <div class="model">
+            <div class="header">
+                <h1 class="title">
+                    Are you sure you want to export this report?
+                </h1>
+            </div>
+            <div class="content">
+                <div class="buttons_container">
+                    <button class="confirm button" id="yes" name="yes">Yes</button>
+                    <button class="deny button" id="no" name="no">No</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="cancelDiv" style="display:none" class="confirm_box">
+    <div class="overlay"></div>
+    <div class="confirm_model">
+        <div class="model">
+            <div class="header">
+                <h1 class="title">
+                    Do you want to leave this page?
+                </h1>
+            </div>
+            <div class="content">
+                <div class="buttons_container">
+                    <button class="confirm button" id="yes_cancel" name="yes_cancel">Yes</button>
+                    <button class="deny button" id="no_cancel" name="no_cancel">No</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    /*
+    If save button is clicked, wait for user to click yes or no button of the pop up window. If user click yes
+    submit form otherwise go to manager_dashboard.html page
+     */
+    $("#export_btn").click(function (event) {
+        event.preventDefault();
+        document.getElementById('exportDiv').style.display = "block";
+        $("#yes").click(function (event) {
+            document.getElementById('exportDiv').style.display = "none";
+            document.getElementById('form_id').submit();
+        });
+        $("#no").click(function (event) {
+            document.getElementById('exportDiv').style.display = "none";
+        });
+    });
+
+    /*
+    If cancel button is clicked, wait for user to click yes or no button of the pop up window. If user click no
+    submit form otherwise go to manager_dashboard.html page
+    */
+    $("#cancel").click(function (event) {
+        event.preventDefault();
+        document.getElementById('cancelDiv').style.display = "block";
+        $("#yes_cancel").click(function (event) {
+            document.getElementById('cancelDiv').style.display = "none";
+            window.location.replace("manager_dashboard.html");
+        });
+        $("#no_cancel").click(function (event) {
+            document.getElementById('cancelDiv').style.display = "none";
+        });
+    });
+</script>
+
+
+</body>
+</html>
+
+
+
+
--- Manager/Average Report/average_per_week_manager.php
+++ Manager/Average Report/average_per_week_manager.php
@@ -1,84 +1,63 @@
-<!--
-- Author: Maria Kouppi
--
-- A manager can can report on the time worked by his employees. The managers give the range of dates the managers
-- want to report, the name of file. If the export button clicked, then select from database the data of his employees
-- concerning their hours of work and create a report on JSON or XML format.
--
--->
 <?php
+/**
+ * Author: Maria Kouppi
+ *
+ * A manager can export a report of the working hours of his employees. The manager gives the range of dates in which he
+ * wants to export the report and the name of the report. If the export button clicked, then a report with the working hours of employees
+ * has been exported in the form of manager's decision(XML or JSON).
+ */
 session_start();
-//ini_set('display_errors',1);
-//error_reporting(E_ALL);
 include('db.php');
 
 // get the values of text box
 $From = $_POST['from_date'];
+$_SESSION['fromDate'] = $From;
 $To = $_POST['to_date'];
+$_SESSION['toDate'] = $_POST['to_date'];
 $file_name = $_POST['file_name'];
 $Username = $_SESSION['username'];
-if (isset($_POST['export_btn'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    //select data from database
-    $getData = "SELECT SUM(TIME_TO_SEC(TIMEDIFF(AttendanceTime.ClockOut,AttendanceTime.ClockIn))-(AttendanceTime.BreakLength*60)) AS SecWorked,AttendanceTime.Username,AttendanceTime.BreakLength,Employee.ID,Employee.Name,Employee.Surname FROM AttendanceTime INNER JOIN Employee ON (AttendanceTime.Username=Employee.Username) WHERE (Date >= '$From' AND Date <= '$To') AND Employee.UsernameManager LIKE '$Username' GROUP BY AttendanceTime.Username";
+    $DateFrom = date("Y-m-d", strtotime($From));
+    $DateTo = date("Y-m-d", strtotime($To));
 
-    $sql_con = mysqli_query($conn, $getData);
-    $employee_data = array();
-    if (!$sql_con) {
+    $DateFrom = date("Y-m-d", strtotime($From));
+    $DateTo = date("Y-m-d", strtotime($To));
+
+    if (strtotime($DateTo) < strtotime($DateFrom)) {
         echo '<script type="text/javascript">
-	window.alert("ERROR CONNECTION WITH DATABASE");
-	window.location.replace("average_per_week.html");
-	</script>';
-        exit();
+			window.alert("The To field has to be after the From field!");
+			window.location.replace("average_per_week.php");
+			</script>';
+    } else if (strtotime($DateFrom) >= strtotime('now')) {
+        echo '<script type="text/javascript">
+			var d = new Date();
+			window.alert("The date must be before " + d.getDate() + "/" + d.getMonth()+1 + "/"  + d.getFullYear()); 
+			window.location.replace("average_per_week.php");
+			</script>';
+    } else if (strtotime($DateTo) >= strtotime('now')) {
+        echo '<script type="text/javascript">
+			var d = new Date();
+			window.alert("The date must be before " + d.getDate() + "/" + d.getMonth()+1 + "/"  + d.getFullYear()); 
+			window.location.replace("average_per_week.php");
+			</script>';
+
     } else {
         //if format which the manager select is XML
         if (strcmp($_POST['format_export'], "XML") == 0) {
-            $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-            $root_element = "Leave_Employee";
-            $xml .= "<$root_element>";
-            if (mysqli_num_rows($sql_con) > 0) {
-                while ($row = mysqli_fetch_assoc($sql_con)) {
-                    $xml .= "<Leave_Employee>";
-
-                    //loop through each key,value pair in row
-                    foreach ($row as $key => $value) {
-                        //$key holds the table column name
-                        $xml .= "<$key>";
-
-                        //embed the SQL data in a CDATA element to avoid XML entity issues
-                        $xml .= "<![CDATA[$value]]>";
-
-                        //and close the element
-                        $xml .= "</$key>";
-                    }
-
-                    $xml .= "</Leave_Employee>";
-                }
-
-                $xml .= "</$root_element>";
-
-                //send the xml header to the browser
-                header("Content-Type:text/xml");
-
-                //output the XML data
-                echo $xml;
-            }
-        } else {
-            //if the format which the manager select is JSON
-            while ($row = mysqli_fetch_array($sql_con)) {
-                $employee_data[] = array('Username' => $row['Username'], 'ID' => $row['ID'], 'Name' => $row['Name'], 'Surname' => $row['Surname'], 'SecWorked' => $row['SecWorked']);
-            }
-            echo json_encode($employee_data);
-        }
-        /*$myfile = fopen('newfile.txt', 'w') or die("can't open file");
-            fwrite($myfile,json_encode($employee_data));
-            fclose($myfile);
-
-            if(file_exists("newfile.txt")){
             echo '<script type="text/javascript">
-            window.alert("Exported successfully");
-            window.location.replace("payroll_report.html");
-            </script>';}*/
+			window.open("xml_file.php","_blank");
+			window.location.replace("manager_dashboard.html");
+			</script>';
+            exit();
+            //if format which the manager select is JSON
+        } else {
+            echo '<script type="text/javascript">
+			window.open("json_file.php","_blank");
+			window.location.replace("manager_dashboard.html");
+			</script>';
+            exit();
+        }
     }
 }
 ?>
