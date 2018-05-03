c947b398e653fd4f1abf0652dc22220f0b3deb22
mkoupp02 <31859288+mkoupp02@users.noreply.github.com>
2018-05-04 01:05:11 +0300

    Add files via upload

--- Manager/View Request/api.php
+++ Manager/View Request/api.php
@@ -1,9 +1,11 @@
 <?php session_start();
 /**
- * Created by PhpStorm.
- * User: ELENI
- * Date: 30/4/2018
- * Time: 2:32 μμ
+ * Author: Maria Kouppi
+ *
+ * If a manager click the "view request" button in his/her dashboard, then in a page, appear a table with all
+ * requests which are pended in database. Click one of the button, "reject" or "accept" or "pending" and the
+ * state of request change in database.
+ *
  */
 include_once 'db.php';
 
@@ -17,9 +19,9 @@ if (isset($_GET['datatable'])) {
     while ($row = mysqli_fetch_array($result)) {
         //save all data in a array
         array_push($employees, array('LeaveID' => $row['LeaveID'], 'Username' => $row['Username'], 'Name' => $row['Name'], 'Surname' => $row['Surname'], 'FromDate' => $row['FromDate'], 'ToDate' => $row['ToDate'], 'Reason' => $row['Reason'],
-            'State' => '<button name="Reject" id="changeReject(' . $row['LeaveID'] . ')">R</button>
-                        <button name="Pending" id="changePending(' . $row['LeaveID'] . ')">P</button>
-                        <button name="Accept" id="changeAccept(' . $row['LeaveID'] . ')">A</button>'));
+            'State' => '<button name="Reject" id="Reject" onclick="changeReject(' . $row['LeaveID'] . ')">R</button>
+                        <button name="Pending" id="Pending" onclick="changePending(' . $row['LeaveID'] . ')">P</button>
+                        <button name="Accept" id="Accept" onclick="changeAccept(' . $row['LeaveID'] . ')">A</button>'));
     }
     $results['data'] = $employees;
     $results['recordsTotal'] = mysqli_num_rows($result);
@@ -28,21 +30,70 @@ if (isset($_GET['datatable'])) {
 }
 if (isset($_GET['reject'])) {
     if (isset ($_POST['id'])) {
-        $id = $_POST['id'];
+        $id = mysqli_real_escape_string($conn,$_POST['id']);
+        //check if is in if
+        $result = array();
         //Update
+        $state = "r";
 
-        /* $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`='r' WHERE `Leave`.`LeaveID` = '$array[$row]'";
-         if (!mysqli_query($conn, $sqlUpdate)) {
-             echo '<script type="text/javascript">
-             window.alert("Your change not insert in Database!" );
-             window.location.replace("manager_view_request.php");
-             </script>';
-             exit();
-         } else {
-             echo '<script type="text/javascript">
-             window.alert("INSERTED CORRECTLY!");
-             window.location.replace("manager_view_request.php");
-             </script>';
-         }*/
+        $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`=? WHERE `Leave`.`LeaveID` = '$id'";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sqlUpdate)) {
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "s", $state);
+            mysqli_stmt_execute($stmt);
+        }
+        $result["status"] = "OK";
+        $result["message"] = "State updated";
+
+        echo json_encode($result);
     }
 }
+if (isset($_GET['accept'])) {
+    if (isset ($_POST['id'])) {
+        $id = $_POST['id'];
+        //check if is in if
+        $result = array();
+        //Update
+
+        $state = "a";
+
+        $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`=? WHERE `Leave`.`LeaveID` = '$id'";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sqlUpdate)) {
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "s", $state);
+            mysqli_stmt_execute($stmt);
+        }
+        $result["status"] = "OK";
+        $result["message"] = "State updated";
+
+        echo json_encode($result);
+    }
+}
+if (isset($_GET['pending'])) {
+    if (isset ($_POST['id'])) {
+        $id = $_POST['id'];
+        //check if is in if
+        $result = array();
+        //Update
+
+        $state = "p";
+
+        $sqlUpdate = "UPDATE `Leave` SET `Leave`.`State`=? WHERE `Leave`.`LeaveID` = '$id'";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sqlUpdate)) {
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "s", $state);
+            mysqli_stmt_execute($stmt);
+        }
+        $result["status"] = "OK";
+        $result["message"] = "State updated";
+
+        echo json_encode($result);
+    }
+}
+
